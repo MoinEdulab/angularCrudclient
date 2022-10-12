e0c4ced4199883edf60d17aa180b285e287a75c2
MoinEdulab <you@example.com>
2022-10-12 16:59:11 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'crudClient';
+  apiUrl = 'http://localhost:3000'
+  constructor(private http: HttpClient){
+ }
+
+  insert(id: any,firstname: any,lastname: any){
+    this.http.post<any[]>(this.apiUrl+"/details",{
+      id:id,
+    firstname:firstname,
+    lastname:lastname}).subscribe(data => 
+      console.log(data));
+
+    console.log("data inserted !");
+    
+  }
+  getData(){
+    this.http.get<any[]>(this.apiUrl+"/details").subscribe(data => 
+      alert(JSON.stringify(data)));
+         }
+       
+         data(id:any){
+          this.http.get<any[]>(this.apiUrl+"/details1?id="+id).subscribe(data => 
+            alert(JSON.stringify(data)));
+      console.log('getid');}
+
+  update(id: any,firstname: any,lastname: any){
+    this.http.put<any[]>(this.apiUrl+"/details?id="+id,{
+      id:id,
+    firstname:firstname,
+    lastname:lastname}).subscribe(data => 
+      console.log(data));
+
+    console.log("data upadeted !");
+    
+  }
+  
+  delete(id:any,){
+    this.http.delete<any[]>(this.apiUrl+"/details?id="+id).subscribe(data => 
+      alert(JSON.stringify(data)));
+console.log('data deleted');
+
+      
+  }
+  
+  
+}
+
+
